584a9c01348aa8e31f47b4896cf1343c3d2bdc44
salmanbashar1009 <salmanbashar101@gmail.com>
2026-05-22 02:24:54 +0600

    feat: add transaction state

---
+++ lib/features/transactions/presentation/bloc/transaction_state.dart
@@ -0,0 +1,64 @@
+
+import 'package:equatable/equatable.dart';
+
+import '../../domain/entities/transaction_entity.dart';
+
+abstract class TransactionState extends Equatable {
+  const TransactionState();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class TransactionInitial extends TransactionState {}
+
+class TransactionLoading extends TransactionState {}
+
+class TransactionLoaded extends TransactionState {
+  final List<TransactionEntity> transactions;
+  final double budget;
+
+  const TransactionLoaded({
+    required this.transactions,
+    this.budget = 5000,
+  });
+
+  /// Derived data from transactions
+  double get totalSpent => transactions
+      .where((t) => t.isExpense)
+      .fold(0.0, (sum, t) => sum + t.amount);
+
+  double get totalIncome => transactions
+      .where((t) => t.isIncome)
+      .fold(0.0, (sum, t) => sum + t.amount);
+
+  /// Daily spending for the line chart
+  Map<DateTime, double> get dailySpending {
+    final now = DateTime.now();
+    final result = <DateTime, double>{};
+
+    for (var i = 6; i >= 0; i--) {
+      final day = DateTime(now.year, now.month, now.day - i);
+      final total = transactions
+          .where((t) =>
+      t.isExpense &&
+          t.date.year == day.year &&
+          t.date.month == day.month &&
+          t.date.day == day.day)
+          .fold(0.0, (sum, t) => sum + t.amount);
+      result[day] = total;
+    }
+    return result;
+  }
+
+  @override
+  List<Object?> get props => [transactions, budget];
+}
+
+class TransactionError extends TransactionState {
+  final String message;
+  const TransactionError(this.message);
+
+  @override
+  List<Object?> get props => [message];
+}
